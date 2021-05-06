e115bc38088ab457c975b36c43220122abe7c11a
KamilShaykhutdinov <kam1l@yandex.ru>
2021-05-06 17:13:52 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+/* Индекс слайда по умолчанию */
+var slideIndex = 1;
+showSlides(slideIndex);
+
+/* Функция увеличивает индекс на 1, показывает следующий слайд*/
+function nextSlide() {
+  if (slideIndex != 5) {
+    return showSlides((slideIndex += 1));
+  }
+}
+
+/* Функция уменьшает индекс на 1, показывает предыдущий слайд*/
+function prevSlide() {
+  if (slideIndex != 1) return showSlides((slideIndex -= 1));
+}
+
+/* Устанавливает текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Основная функция слайдера */
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("item");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  slides[slideIndex - 1].style.display = "block";
+}
